977631304910dd5f0cc25ae48d460a3483828d6e
Lav Pandey <107080085+iDataGuy@users.noreply.github.com>
2024-08-14 20:28:30 +0100

    Update locals.tf

--- modules/grants/locals.tf
+++ modules/grants/locals.tf
@@ -7,12 +7,12 @@ locals {
 
   # Standard privileges for RW and RO roles
   standard_privileges = {
-    RW_ROLE = sort(["CREATE SEQUENCE", "CREATE VIEW", "CREATE FILE FORMAT", "CREATE TAG", "CREATE TASK", "USAGE", "CREATE FUNCTION", "CREATE EXTERNAL TABLE", "CREATE PROCEDURE", "CREATE STAGE", "CREATE TABLE", "CREATE TEMPORARY TABLE", "CREATE PIPE", "MODIFY", "CREATE DYNAMIC TABLE", "MONITOR", "CREATE STREAMLIT"])
-    RO_ROLE = sort(["USAGE", "SELECT"])
+    RW_ROLE = sort(["CREATE SEQUENCE", "CREATE VIEW", "CREATE FILE FORMAT", "CREATE TAG", "CREATE TASK", "USAGE", "CREATE FUNCTION", "CREATE EXTERNAL TABLE", "CREATE PROCEDURE", "CREATE STAGE", "CREATE TABLE", "CREATE TEMPORARY TABLE", "CREATE PIPE", "MODIFY", "CREATE DYNAMIC TABLE", "MONITOR"])
+    RO_ROLE = sort(["USAGE"])
   }
 
-  # Generate privileges for each role, schema, and database based on variables.yml input
-  merged_privileges_list = flatten([
+  # Generate schema-level privileges based on variables.yml input
+  merged_privileges_list = distinct(flatten([
     for v in local.variables.schema_privileges : [
       for schema in sort(v.schemas) : [
         for priv in local.standard_privileges[v.role_category] : [
@@ -25,34 +25,21 @@ locals {
         ]
       ]
     ]
-  ])
+  ]))
 
-  # Convert merged_privileges_list to a map with unique keys
-  merged_privileges = { for v in local.merged_privileges_list : "${v.role}_${v.database}_${v.schema}_${v.privilege}" => v }
+  # Convert merged_privileges_list to a map with unique keys for schema-level grants
+  merged_privileges = {
+    for v in local.merged_privileges_list :
+    "${v.role}_${v.database}_${v.schema}_${v.privilege}" => v
+  }
 
-  # Default database-level privileges (USAGE)
-  privilegemaps_db_list = flatten([
-    for v in local.variables.schema_privileges : [
-      for role in sort(v.role) : {
-        privilege = "USAGE"
-        role      = role
-        database  = "${local.env}_${v.database}"
-      }
-    ]
-  ])
-
-  # Convert the list to a map with proper keys
-  privilegemaps_db = { for v in local.privilegemaps_db_list : "${v.role}_${v.database}_${v.privilege}" => {
-    role      = v.role,
-    database  = v.database,
-    privilege = v.privilege
-  } }
-
-  # Role hierarchy
+  # Role hierarchy, removing duplicates
   role_hierarchy = {
-    for v in local.variables.role_hierarchy : "${v.parent_role}_${v.child_role}_${local.env}" => {
+    for v in distinct(local.variables.role_hierarchy) :
+    "${v.parent_role}_${v.child_role}_${local.env}" => {
       parent_role = "${v.parent_role}"
       child_role  = "${v.child_role}"
     }
   }
 }
+
